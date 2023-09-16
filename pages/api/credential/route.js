25f948da05ec80f57f5a7a9b88b7ea272ff80e3e
Daksh <akshd@outlook.com>
2023-09-16 09:12:47 +0530

    fix: refactor code to next13

--- pages/api/credential/route.js
+++ pages/api/credential/route.js
@@ -1,6 +1,5 @@
 import { EAS } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
-import { NextResponse } from "next/server";
 const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
 const eas = new EAS(EASContractAddress);
 const provider = new ethers.providers.JsonRpcProvider(
@@ -8,17 +7,13 @@ const provider = new ethers.providers.JsonRpcProvider(
 );
 eas.connect(provider);
 
-export async function POST(req) {
-  const { uid, signature } = await req.json();
+export default async function handler(req, res) {
+  const { uid, signature } = await req.body;
   const attestation = await eas.getAttestation(uid);
-  const signer = ethers.utils.recoverAddress(attestation.data, signature);
-  const hash = ethers.utils.keccak256(signer);
-  console.log(signer);
-  console.log("attestation", attestation);
-  return NextResponse.json(
-    { status: "ok", hash },
-    {
-      status: 200,
-    }
-  );
+  const signer = ethers.utils.verifyMessage(uid, signature);
+  const status = signer === attestation.recipient
+  res.status(200).send({
+    signer,
+    status
+  })
 }
