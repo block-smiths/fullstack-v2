54a3d369b4f018e91413cc78d647030c068de7a2
darshil89 <darshilmahraur67@gmail.com>
2023-09-16 13:40:31 +0530

    component bna diye hai

---
+++ pages/getFiles.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import getFileCard from "../components/getFileCard";
+
+const getFiles = () => {
+  const handleGet = async (e) => {};
+  return (
+    <div>
+      <div className="bg-grey-600 py-4 px-6 flex justify-between items-center w-full mb-4">
+        <div className="flex items-center justify-center w-full">
+          <h1 className="text-5xl text-yellow-500 py-4 text-center font-bold ml-40">
+            E-Vault
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default getFiles;
